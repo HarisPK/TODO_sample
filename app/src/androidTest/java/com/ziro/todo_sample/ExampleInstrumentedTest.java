6c604e390128a8c2b42eb89bab82865c2a8a108b
haris93 <haris93pk@gmail.com>
2019-09-22 23:29:25 +0530

    Experimental unit test case for Room Database insertion

--- app/src/androidTest/java/com/ziro/todo_sample/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/ziro/todo_sample/ExampleInstrumentedTest.java
@@ -1,12 +1,21 @@
 package com.ziro.todo_sample;
 
+import android.arch.persistence.room.Room;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.ziro.todo_sample.models.ToDo;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.IOException;
+import java.util.List;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
 import static org.junit.Assert.*;
 
 /**
@@ -16,11 +25,28 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+    private ToDoDao userDao;
+    private AppDatabase db;
 
-        assertEquals("com.ziro.todo_sample", appContext.getPackageName());
+    @Before
+    public void createDb() {
+        Context context = getApplicationContext();
+        db = Room.inMemoryDatabaseBuilder(context, AppDatabase.class).build();
+        userDao = db.toDoDao();
+    }
+
+    @After
+    public void closeDb() throws IOException {
+        db.close();
+    }
+
+    @Test
+    public void writeUserAndReadInList() throws Exception {
+        ToDo toDo=new ToDo();
+        toDo.setTitle("Haris");
+        toDo.setDesc("ZIRO");
+        userDao.insertNewToDo(toDo);
+        List<ToDo> byName = userDao.getAllTodo();
+        assertEquals(byName.get(0).getTitle(), "Haris");
     }
 }
